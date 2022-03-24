464c2f39a30fa7a7644fdd3708c645d5bfb2208c
Guillermo <duche27@hotmail.com>
2022-03-24 15:47:55 +0100

    Fetch Payment Details Nuevo micro UserService

--- src/main/java/com/gui/estore/ordersservice/OrdersServiceApplication.java
+++ src/main/java/com/gui/estore/ordersservice/OrdersServiceApplication.java
@@ -1,14 +1,11 @@
 package com.gui.estore.ordersservice;
 
-import com.gui.estore.ordersservice.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//@Import({ AxonConfig.class })
 public class OrdersServiceApplication {
 
     public static void main(String[] args) {
