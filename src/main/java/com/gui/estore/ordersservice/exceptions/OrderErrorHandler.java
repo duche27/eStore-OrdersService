d17f1431632d48ee0e214b963f20690c6ea8673f
Guillermo Fernández <guillermo_fernandez_raboso@carrefour.com>
2022-04-13 09:33:12 +0200

    exceptions corregidas

--- src/main/java/com/gui/estore/ordersservice/exceptions/OrderErrorHandler.java
+++ src/main/java/com/gui/estore/ordersservice/exceptions/OrderErrorHandler.java
@@ -1,6 +1,7 @@
 package com.gui.estore.ordersservice.exceptions;
 
 import org.axonframework.commandhandling.CommandExecutionException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +20,13 @@ public class OrderErrorHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.NO_CONTENT);
     }
 
-    @ExceptionHandler(value = {RuntimeException.class})
-    public ResponseEntity<ErrorMessage> handleRuntimeException(RuntimeException e) {
+    // para manejar excepciones de validaciones de hibernate
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Error> fieldsRestValidationHandler(DataIntegrityViolationException e) {
 
-        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
-        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        Error error = new Error(e.getMostSpecificCause().getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     // excepciones del AGGREGATE
@@ -36,6 +39,13 @@ public class OrderErrorHandler {
 
     @ExceptionHandler(value = {IllegalStateException.class})
     public ResponseEntity<ErrorMessage> handleIllegalStateException(IllegalStateException e) {
+
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    public ResponseEntity<ErrorMessage> handleRuntimeException(RuntimeException e) {
 
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
