e30f018e2bc5901e6c864a923663575aac670804
Guillermo Fernández <guillermo_fernandez_raboso@carrefour.com>
2022-04-20 11:56:16 +0200

    actualizamos dependencias:  - AXON: 4.5.9  - EUREKA-CLIENT: 3.1.1  - spring-boot-starter-validation: 2.6.6 nuevos micro-servicios:  - SHIPMENT  - NOTIFICATIONS

--- src/main/java/com/gui/estore/ordersservice/exceptions/ProductServiceEventErrorHandler.java
+++ src/main/java/com/gui/estore/ordersservice/exceptions/ProductServiceEventErrorHandler.java
@@ -4,7 +4,7 @@ import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.EventMessageHandler;
 import org.axonframework.eventhandling.ListenerInvocationErrorHandler;
 
-public class ProductServiceEventHandler implements ListenerInvocationErrorHandler {
+public class ProductServiceEventErrorHandler implements ListenerInvocationErrorHandler {
     @Override
     public void onError(Exception exception, EventMessage<?> eventMessage, EventMessageHandler eventMessageHandler) throws Exception {
         throw exception;
